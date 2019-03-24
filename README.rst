54f25c61e594da9ffd5684f64e187306fe280510
Daniel Kuruc <dnk@dnk.net.pl>
2019-03-24 14:13:48 +0100

    Update fan_controller
    - Use PID Controller instead custom solution
    - Add simulation mode
    - Cleanup with settings
    - Add settings.local.ini support

--- README.rst
+++ README.rst
@@ -1,31 +1,53 @@
 SUPER  NESPI CASE
 =================
 
+Tools Included
+--------------
+1) Temperature Fan Controller
+
 
 Requirements
 ------------
+
+Common
+~~~~~~
 - `Raspberry Pi <https://www.raspberrypi.org/>`_
 - `Super NESPi Case <http://snespi.com/>`_ (Important, **not** `Retroflag Case <http://retroflag.com/SUPERPi-CASE-J.html>`_!)
 - Python 2.7
 - `RPi.GPIO <https://sourceforge.net/projects/raspberry-gpio-python/>`_
 
+Temperature Fan Controller
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+- `mock <https://pypi.org/project/mock/>`_ Python package (for simulation mode)
+- `simple-pid <https://pypi.org/project/simple-pid/>`_ Python package
 
 
 Installation
 ------------
-All included scripts requires ``settings.ini`` file to be present in ``/home/pi/snespi/`` directory.
+1. Install required Python packages::
+
+      pip install -r requirements.txt
+2. All included scripts requires ``settings.ini`` file to be present in ``/home/pi/snespi/`` directory.
+
+   | You can override default settings, by creating file ``settings.local.ini``, next to included ``settings.ini`` and selectively placing there your override values (BE CAREFUL! Setting incorrect settings may lead to board damage!)
+
 
 Temperature Fan Controller
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
-1) Copy file ``fan_controller.py`` to ``/home/pi/snespi/`` directory
-2) Modify ``settings.ini`` if needed (Warning! Setting incorrect settings may lead to board damage!)
-3) Configure script autostart
-    a) Edit ``rc.local`` file.
-    b) Insert following code before ``exit 0`` (``exit 0`` must be a last line in file):
-        .. code-block:: bash
+1. Copy file following files to ``/home/pi/snespi/`` directory on your Raspberry Pi:
 
-            /usr/bin/python2.7 /home/pi/snespi/fan_controller.py
-    c) Reboot: ``sudo reboot``
+   - ``fan_controller.py``
+   - ``settings.ini``
+2. Configure script autostart
+
+   a. Run::
+
+         crontab -e
+   b. Add new entry to crontab configuraion::
+
+         @reboot /usr/bin/python2.7 /home/pi/snespi/fan_controller.py & > /dev/null 2>&1
+   c. Save file & close editor
+   d. Reboot your RPi: ``sudo reboot``
 
 
 License
--- snespi/fan_controller.py
+++ snespi/fan_controller.py
@@ -1,127 +1,266 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
+import argparse
 import os
 import time
 import sys
+import warnings
 
-import ConfigParser
-import RPi.GPIO as GPIO
+from ConfigParser import SafeConfigParser
+
+try:
+    import RPi.GPIO as GPIO
+except (ImportError, RuntimeError):
+    print '---------------------------------------------------------------'
+    print 'Running outside Raspberry Pi, GPIO functions has been disabled!'
+    print '---------------------------------------------------------------'
+    GPIO = False
+
+warnings.simplefilter('ignore')
+from simple_pid import PID  # noqa: E402  # pylint:disable=wrong-import-position
+
+# Base constants
 
 PWD = os.path.dirname(os.path.realpath(__file__))
-TEMPERATURE_ACCURACY = 3
-PWM_ACCURACY = 1
+GPIO_PINS = (12, 32, 33)
+PWM_MIN, PWM_FULL = 0, 100
+REAL_PWM_100, REAL_PWM_0 = 0, 100
 
-PWM_OFF = 100
-PWM_ON = 0
+# Read configuration from ini file(s)
 
+parser = SafeConfigParser()
+parser.read([
+    os.path.join(PWD, 'settings.ini'),
+    os.path.join(PWD, 'settings.local.ini'),
+])
 
-class Configuration(object):
-    @property
-    def debug(self):
-        return bool(int(self._config.get('General', 'Debug')))
+SLEEP_TIME = parser.getint('General', 'sleep_time')
 
-    @property
-    def fan_pin(self):
-        return self._int('FanPin')
+FAN_TEMPERATURE_ON = parser.getint('Fan', 'temperature_on')
+FAN_SPEED_MIN_PWM = parser.getint('Fan', 'speed_min_pwm')
+FAN_SPEED_MAX_PWM = parser.getint('Fan', 'speed_max_pwm')
 
-    @property
-    def sleep_time(self):
-        return self._float('SleepTime')
+PID_KP = parser.getfloat('Fan', 'pid_kp')
+PID_KI = parser.getfloat('Fan', 'pid_ki')
+PID_KD = parser.getfloat('Fan', 'pid_kd')
 
-    @property
-    def pwm_frequency(self):
-        return self._int('PwmFrequency')
+FAN_GPIO_PIN = parser.getint('Fan', 'gpio_pin')
+PWM_FREQUENCY = parser.getint('Fan', 'pwm_frequency')
 
-    @property
-    def pwm_steps(self):
-        return self._int('PwmSteps')
-
-    @property
-    def fan_temperature_on(self):
-        return self._float('FanTemperatureOn', TEMPERATURE_ACCURACY)
-
-    @property
-    def fan_temperature_full_speed(self):
-        return self._float('FanTemperatureFullSpeed', TEMPERATURE_ACCURACY)
-
-    @property
-    def fan_speed_min(self):
-        return self._int('FanSpeedMin')
-
-    @property
-    def fan_speed_max(self):
-        return self._int('FanSpeedMax')
-
-    def __init__(self):
-        self._config = ConfigParser.SafeConfigParser()
-        self._config.read(os.path.join(PWD, 'settings.ini'))
-
-    def get_steps(self):
-        temperature_step = float(self.fan_temperature_full_speed - self.fan_temperature_on) / self.pwm_steps
-        pwm_step = float(self.fan_speed_max - self.fan_speed_min)/self.pwm_steps
-
-        temperatures = (self.fan_temperature_on + (i * temperature_step) for i in range(1, self.pwm_steps + 1))
-        pwms = (self.fan_speed_min + (i*pwm_step) for i in range(1, self.pwm_steps + 1))
-
-        return tuple(zip(
-            [round(item, TEMPERATURE_ACCURACY) for item in temperatures],
-            [round(item, PWM_ACCURACY) for item in list(pwms)[::-1]],
-        ))
-
-    def _float(self, option, rounding=2):
-        return round(float(self._config.get('Fan', option)), rounding)
-
-    def _int(self, option):
-        return int(self._config.get('Fan', option))
+DEFAULT_SIMULATION_TEMP_MIN = FAN_TEMPERATURE_ON
+DEFAULT_SIMULATION_TEMP_MAX = int(round(FAN_TEMPERATURE_ON * 1.5))
+DEFAULT_SIMULATION_CYCLES = 1
 
 
 class FanController(object):
-    def __init__(self):
-        self.configuration = Configuration()
-        self.debug = self.configuration.debug
-        self.steps = self.configuration.get_steps()
-        self._init_gpio()
+    def __init__(self, verbose=None):
+        self._verify_config()
+
+        self.verbose = verbose or False
+
+        self.fan = None
+        self.pid = PID(
+            Kp=PID_KP, Ki=PID_KI, Kd=PID_KD,
+            setpoint=-1 * FAN_TEMPERATURE_ON,
+            sample_time=SLEEP_TIME,
+            output_limits=(FAN_SPEED_MIN_PWM, FAN_SPEED_MAX_PWM),
+        )
+        if GPIO:
+            self._init_gpio()
 
     def start(self):
-        if self.debug:
-            print('Starting fan controller')
-        self.fan.start(PWM_OFF)
+        if self.verbose:
+            print 'Starting fan controller'
+        if GPIO:
+            self.fan.start(REAL_PWM_0)
 
         while True:
             cpu_temperature = self._get_temperature()
-            pwm_to_set = PWM_OFF
+            duty_cycle = self.pid(-1 * cpu_temperature)
 
-            if cpu_temperature > self.configuration.fan_temperature_on:
-                # Looking for lowest pwm for current CPU temperature
-                for step_temperature, step_pwm in self.steps:
-                    pwm_to_set = step_pwm
-                    if step_temperature >= cpu_temperature:
-                        break
+            if duty_cycle == FAN_SPEED_MIN_PWM:
+                duty_cycle = PWM_MIN
 
-            if self.debug:
-                print('Temperature: {} PWM: {}'.format(cpu_temperature, pwm_to_set))
-            self.fan.ChangeDutyCycle(pwm_to_set)
-            time.sleep(self.configuration.sleep_time)
+            fan_speed = '%d%%' % duty_cycle
+            # Reversing PWM, because 0 = fan full speed / 100 - fan stopped
+            duty_cycle = 100 - duty_cycle
+
+            if self.verbose:
+                print 'Temperature: %d°C : Fan Speed: %s' % (cpu_temperature, fan_speed)
+
+            if GPIO:
+                self.fan.ChangeDutyCycle(duty_cycle)
+
+            time.sleep(SLEEP_TIME)
+
+    def simulate(self, temp_min=None, temp_max=None, cycles=None):
+        print '********** Running simulation mode **********'
+
+        temp_min = temp_min or DEFAULT_SIMULATION_TEMP_MIN
+        temp_max = temp_max or DEFAULT_SIMULATION_TEMP_MAX
+        cycles = cycles or DEFAULT_SIMULATION_CYCLES
+
+        from mock import Mock
+        temperatures = list(range(temp_min, temp_max))
+        temperatures = temperatures + temperatures[::-1]
+        self._get_temperature = Mock(side_effect=temperatures * cycles)
+        self.start()
 
     def stop(self):
-        if self.debug:
-            print('Stopping fan controller')
-        GPIO.cleanup()
+        if self.verbose:
+            print 'Stopping fan controller'
+        if GPIO:
+            self.fan.ChangeDutyCycle(REAL_PWM_0)
+            GPIO.cleanup()
 
-    def _get_temperature(self):
+    def _get_temperature(self):  # pylint:disable=method-hidden
+        if not GPIO:
+            return 50
         with open('/sys/class/thermal/thermal_zone0/temp', 'r') as fp:
-            return round(float(fp.read()) / 1000, TEMPERATURE_ACCURACY)
+            return round(float(fp.read()) / 1000)
 
     def _init_gpio(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.configuration.fan_pin, GPIO.OUT, initial=GPIO.HIGH)
-        self.fan = GPIO.PWM(self.configuration.fan_pin, self.configuration.pwm_frequency)
+        GPIO.setup(FAN_GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH)
+        self.fan = GPIO.PWM(FAN_GPIO_PIN, PWM_FREQUENCY)
+
+    def _verify_config(self):
+        assert SLEEP_TIME > 0, 'Setting \'sleep_time\' must be > 0'
+        assert FAN_TEMPERATURE_ON > 0, 'Setting \'temperature_on\' must be > 0'
+        assert PWM_FREQUENCY > 0, 'Setting \'pwm_frequency\' must be > 0'
+
+        assert PID_KP >= 0, 'Setting \'pid_kp\' must be >= 0'
+        assert PID_KI >= 0, 'Setting \'pid_ki\' must be >= 0'
+        assert PID_KD >= 0, 'Setting \'pid_kd\' must be >= 0'
+
+        assert PWM_MIN <= FAN_SPEED_MIN_PWM <= PWM_FULL, (
+            'Setting \'speed_min_pwm\' must have value between %d - %s' % (PWM_MIN, PWM_FULL)
+        )
+        assert PWM_MIN <= FAN_SPEED_MAX_PWM <= PWM_FULL, (
+            'Setting \'speed_max_pwm\' must have value between %d - %s' % (PWM_MIN, PWM_FULL)
+        )
+        assert FAN_SPEED_MIN_PWM < FAN_SPEED_MAX_PWM, (
+            'Setting \'speed_min_pwm\' must be lower than \'speed_max_pwm\''
+        )
+        assert FAN_GPIO_PIN in GPIO_PINS, (
+            'Setting \'gpio_pin\' must be one of %s' % (GPIO_PINS,)
+        )
 
 
 if __name__ == '__main__':
-    controller = FanController()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Increase output verbosity',
+    )
+
+    # Simulation options
+    subparsers = parser.add_subparsers()  # pylint:disable=invalid-name
+    subparsers.required = False
+
+    simulation = subparsers.add_parser(  # pylint:disable=invalid-name
+        'simulation',
+        help=(
+            'Simulation will REALLY RUN your fan with given settings. '
+            'This mode generates fake temperature reads in given number of \'cycles\'. '
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    simulation.add_argument(
+        '--temp-min',
+        help='[°C] Min temperature in cycle',
+        type=int, default=DEFAULT_SIMULATION_TEMP_MIN,
+    )
+    simulation.add_argument(
+        '--temp-max',
+        help='[°C] Max temperature in cycle',
+        type=int, default=DEFAULT_SIMULATION_TEMP_MAX,
+    )
+    simulation.add_argument(
+        '--cycles',
+        help='Number of repeat cycles of simulation',
+        type=int, default=DEFAULT_SIMULATION_CYCLES,
+    )
+
+    simulation.add_argument(
+        '--sleep-time',
+        help='[seconds] Time between temperature checks',
+        type=int, default=SLEEP_TIME,
+    )
+    simulation.add_argument(
+        '--temperature-on',
+        help='[°C] Temperature at which the fan will be turned on',
+        type=int, default=FAN_TEMPERATURE_ON,
+    )
+    simulation.add_argument(
+        '--speed-min-pwm',
+        help='[0-100%%] Minimum fan speed',
+        type=int, default=FAN_SPEED_MIN_PWM,
+    )
+    simulation.add_argument(
+        '--speed-max-pwm',
+        help='[0-100%%] Maximimum fan speed',
+        type=int, default=FAN_SPEED_MAX_PWM,
+    )
+    simulation.add_argument(
+        '--pid-kp',
+        help='The value for the proportional gain Kp of PID Controller',
+        type=float, default=PID_KP,
+    )
+    simulation.add_argument(
+        '--pid-ki',
+        help='The value for the integral gain Ki of PID Controller',
+        type=float, default=PID_KI,
+    )
+    simulation.add_argument(
+        '--pid-kd',
+        help='The value for the derivative gain Kd of PID Controller',
+        type=float, default=PID_KD,
+    )
+
+    simulation.add_argument(
+        '--pwm-frequency',
+        help='[Hz] PWM switching frequency',
+        type=float, default=PWM_FREQUENCY,
+    )
+
+    # Because there is no optional subparser in Python 2 - https://bugs.python.org/issue9253
+    def _get_options():
+        args = sys.argv[1:]
+        simulation_mode = 'simulation' in args
+        if not simulation_mode:
+            args.append('simulation')
+
+        parser_options = parser.parse_args(args=args)
+        parser_options.simulation_mode = simulation_mode
+        return parser_options
+
+    options = _get_options()  # pylint:disable=invalid-name
+
+    if options.simulation_mode:
+        SLEEP_TIME = options.sleep_time
+        FAN_TEMPERATURE_ON = options.temperature_on
+        FAN_SPEED_MIN_PWM = options.speed_min_pwm
+        FAN_SPEED_MAX_PWM = options.speed_max_pwm
+        PID_KP = options.pid_kp
+        PID_KI = options.pid_ki
+        PID_KD = options.pid_kd
+        PWM_FREQUENCY = options.pwm_frequency
+
+    controller = FanController(verbose=options.verbose or options.simulation_mode)  # pylint:disable=invalid-name
     try:
-        controller.start()
-    except:
+        if options.simulation_mode:
+            controller.simulate(
+                temp_min=options.temp_min,
+                temp_max=options.temp_max,
+                cycles=options.cycles,
+            )
+        else:
+            controller.start()
+    finally:
         controller.stop()
         sys.exit()
